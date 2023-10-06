dfa7d0c1daba1b7a10d2a0e0d2bfd79e4ae71b1d
Tommy Mathisen <tommy.mathisen@aland.net>
2023-10-06 21:03:42 +0300

    wxgtk fix

--- bootable_usb_creator.py
+++ bootable_usb_creator.py
@@ -72,7 +72,7 @@ class BootableUSBCreator(tk.Tk):
         """Install WoeUSB using the package manager."""
         # For simplicity, Debian/Ubuntu/mint installation steps are provided. (will add more distros later)
         commands = [
-            ["pkexec", "apt", "install", "git", "p7zip-full", "grub2-common", "grub-pc-bin", "parted", "dosfstools", "ntfs-3g"],
+            ["pkexec", "apt", "install", "git", "python3-wxgtk4.0", "p7zip-full", "grub2-common", "grub-pc-bin", "parted", "dosfstools", "ntfs-3g"],
             ["pkexec", "pip3", "install", "WoeUSB-ng"]
         ]
         for cmd in commands:
